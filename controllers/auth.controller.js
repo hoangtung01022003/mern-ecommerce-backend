0fdd7d4478fd67cd1f08e117059bc64e49755b3c
“hoangtung01022003” <hoangtung01022003@gmail.com>
2025-03-13 01:53:30 +0700

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,14 +21,14 @@ const storeRefreshToken = async (userId, refreshToken) => {
 const setCookies = (res, accessToken, refreshToken) => {
 	res.cookie("accessToken", accessToken, {
 		httpOnly: true, // prevent XSS attacks, cross site scripting attack
-		secure: "none",
-		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "none", // prevents CSRF attack, cross-site request forgery attack
 		maxAge: 15 * 60 * 1000, // 15 minutes
 	});
 	res.cookie("refreshToken", refreshToken, {
 		httpOnly: true, // prevent XSS attacks, cross site scripting attack
-		secure: "none",
-		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "none", // prevents CSRF attack, cross-site request forgery attack
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 };
@@ -123,8 +123,8 @@ export const refreshToken = async (req, res) => {
 
 		res.cookie("accessToken", accessToken, {
 			httpOnly: true,
-			secure: "none",
-			sameSite: "strict",
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "none",
 			maxAge: 15 * 60 * 1000,
 		});
 
